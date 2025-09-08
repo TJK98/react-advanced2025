f02f76d2cc964ab740114275edb5311852358c85
TJK98 <wtj1998@naver.com>
2025-09-08 09:48:39 +0900

    카트 장바구니 모달 열고 닫는 기능 추가
    - App 컴포넌트에 장바구니 모달 상태 및 핸들러 함수 추가
    - Header에 props로 모달 열기 기능 전달
    - Cart 및 CartModal 컴포넌트에 닫기 관련 props 전달 및 동작 추가
    - HeaderCartButton 클릭 시 모달 열기 기능 구현

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import CartIcon from './CartIcon.jsx';
+import CartIcon from './CartIcon';
 import styles from './HeaderCartButton.module.scss';
 
-const HeaderCartButton = () => {
-  const { button, icon, badge } = styles;
+const HeaderCartButton = ({ onShow }) => {
+    const { button, icon, badge } = styles;
 
-  return (
-    <button className={button}>
+    return (
+        <button className={button} onClick={onShow}>
       <span className={icon}>
         <CartIcon />
       </span>
-      <span>My Cart</span>
-      <span className={badge}>3</span>
-    </button>
-  );
+            <span>My Cart</span>
+            <span className={badge}>3</span>
+        </button>
+    );
 };
 
 export default HeaderCartButton;
