e3662101ee1db0fbf20584a324d336072813c235
TJK98 <wtj1998@naver.com>
2025-09-08 09:32:51 +0900

    가변 props 전달
    - props를 통해 동적으로 input 속성을 설정하도록 수정
    - MealItemForm에서 Input 컴포넌트 사용 방식 변경

--- src/components/Meals/MealItemForm.jsx
+++ src/components/Meals/MealItemForm.jsx
@@ -4,17 +4,40 @@ import styles from './MealItemForm.module.scss';
 const MealItemForm = (props) => {
   return (
     <form className={styles.form}>
-      <Input
-        label='수량'
-        // input={{
-        //   id: 'amount_' + props.id,
-        //   type: 'number',
-        //   min: '1',
-        //   max: '5',
-        //   step: '1',
-        //   defaultValue: '1',
-        // }}
-      />
+        {/*재사용 가능한 가변 props 객체로 전달하기*/}
+        <Input
+            // label='수량'
+            // x={{
+            //     type: 'range',
+            //     onChange: () => console.log('change!!'),
+            //     min: 1,
+            //     max: 5
+            // }}
+            inputAttr={{
+                id: 'amount_' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1',
+            }}
+        />
+      {/*<Input*/}
+      {/*  label='수량'*/}
+      {/*  // input={{*/}
+      {/*  //   id: 'amount_' + props.id,*/}
+      {/*  //   type: 'number',*/}
+      {/*  //   min: '1',*/}
+      {/*  //   max: '5',*/}
+      {/*  //   step: '1',*/}
+      {/*  //   defaultValue: '1',*/}
+      {/*  // }}*/}
+      {/*    x={{*/}
+      {/*        type: 'text',*/}
+      {/*        onFoucus: () => console.log(`focus!`),*/}
+      {/*        value: 'Hello'*/}
+      {/*    }}*/}
+      {/*/>*/}
       <button>담기</button>
     </form>
   );
--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Input.module.scss';
 
-const Input = ({ label }) => {
+// 재사용 가능한 컴포넌트로 만들고 싶음.
+const Input = ({ label, inputAttr }) => {
   return (
     <div className={styles.input}>
       <label>{label}</label>
-      <input />
+      {/*<input type='text' />*/}
+      {/*<input type='password' />*/}
+      {/*<input type='date' />*/}
+
+      {/*  재사용 가능한 컴포넌트*/}
+        <input {...inputAttr} />
     </div>
   );
 };
